84c90387d5696446ab54000094f99ae4d3581ddf
S5630055 <117213899+S5630055@users.noreply.github.com>
2023-02-10 16:18:59 +0100

    Update motorX.c

--- src/motorX.c
+++ src/motorX.c
@@ -10,14 +10,14 @@
 #include <math.h>
 #include <time.h>
 
-// initializing the hoist position on z axis and velocity of the motor z
-float Zvel=0;
-float ZvelPlus=0;
-float Zpos=0;
+// initializing the hoist position on x axis and velocity of the motor x 
+float Xvel=0;
+float XvelPlus=0;
+float Xpos=0;
 
-// setting them up
-const float Zmin=0;
-const float Zmax=10;
+// seting them up
+const float Xmin=0;
+const float Xmax=40;
 const float Vmax=2;
 
 int log_fd;
@@ -38,15 +38,15 @@ void WriteLog(char * msg){
     }
 }
 
-// creating a function to send the position data of motor z to the world by using the named pipe 
-void SendPosition(const char * myfifo, float Zpos){
+// creating a function to send the position data of motor x to the world by using the named pipe 
+void SendPosition(const char * myfifo, float Xpos){
     int fd;
     if ((fd = open(myfifo, O_WRONLY))==-1){
         close(log_fd);
         perror("Error opening fifo");
         exit(1);
     }
-    if(write(fd, &Zpos, sizeof(Zpos))==-1){
+    if(write(fd, &Xpos, sizeof(Xpos))==-1){
         close(fd);
         close(log_fd);
         perror("Error in writing function");
@@ -56,7 +56,7 @@ void SendPosition(const char * myfifo, float Zpos){
 }
 
 // creating a function to make the velocity calculation 
-float VelCalculator(const char * myfifo, int command, float Zvel){
+float VelCalculator(const char * myfifo, int command, float Xvel){
     int fd;
     if(fd = open(myfifo, O_RDONLY|O_NDELAY)){
         if(read(fd, &command, sizeof(command))==-1){
@@ -66,13 +66,13 @@ float VelCalculator(const char * myfifo, int command, float Zvel){
             exit(1);
         }
         if(command==0){
-            WriteLog("Vz-- button pressed.");
+            WriteLog("Vx-- button pressed.");
             return -1.0;
         } else if(command==1){
-            WriteLog("Vz++ button pressed.");
+            WriteLog("Vx++ button pressed.");
             return 1.0;
         } else if(command==2){
-            WriteLog("Vz stop button pressed.");
+            WriteLog("Vx stop button pressed.");
             return 2.0;
         } else {
             return 0.0;
@@ -86,8 +86,8 @@ float VelCalculator(const char * myfifo, int command, float Zvel){
 // Creating the Emergency Stop signal handler
 void stop_handler(int sig){
     if(sig==SIGUSR1){
-        // Stop the motor z 
-        Zvel=0;
+        // Stop the motor x
+        Xvel=0;
 
         // Listening the stop signal
         if(signal(SIGUSR1, stop_handler)==SIG_ERR){
@@ -99,8 +99,8 @@ void stop_handler(int sig){
 // Creating the Reset signal handler
 void reset_handler(int sig){
     if(sig == SIGUSR2){
-        // Setting the motor z velocity as -2
-        Zvel=-2;
+        // Setting the motor x velocity as -5
+        Xvel=-5;
         
         // Listening the stop signal
         if(signal(SIGUSR2, reset_handler)==SIG_ERR){
@@ -112,24 +112,24 @@ void reset_handler(int sig){
 int main(int argc, char const *argv[]){
     // initializing the named pipes 
     int fd;
-    char * VzFifo = "/tmp/VzFifo";
-    mkfifo(VzFifo, 0666);
-    char * PzFifo = "/tmp/PzFifo";
-    mkfifo(PzFifo, 0666);
+    char * VxFifo = "/tmp/VxFifo";
+    mkfifo(VxFifo, 0666);
+    char * PxFifo = "/tmp/PxFifo";
+    mkfifo(PxFifo, 0666);
 
     int command = -1;
 
-    // Opening the log file and writing to the log file 
-    if ((log_fd = open("log/motorZ.log",O_WRONLY|O_APPEND|O_CREAT, 0666))==-1){
-        perror("Error opening motorZ log file.");
+    // Openinng the log file and writing to the log file 
+    if ((log_fd = open("log/motorX.log",O_WRONLY|O_APPEND|O_CREAT, 0666))==-1){
+        perror("Error opening motorX log file.");
         return 1;
     }
 
     // Listening the signals
     if(signal(SIGUSR1, stop_handler)==SIG_ERR || signal(SIGUSR2, reset_handler)==SIG_ERR){
         // Close file descriptors
-        close(VzFifo);
-        close(PzFifo);
+        close(VxFifo);
+        close(PxFifo);
         close(log_fd);
         exit(1);
     }
@@ -137,31 +137,33 @@ int main(int argc, char const *argv[]){
     while(1){
         time(&rawtime);
         info = localtime(&rawtime);
-        
-        // calculation of velocity and positon of z 
-        ZvelPlus = VelCalculator(VzFifo, command, Zvel); 
-        if(ZvelPlus==2){
-            Zvel=0;
-            ZvelPlus=0;
-        } else {
-            Zvel=Zvel+ZvelPlus;
-            if (Zvel<-Vmax){
-                Zvel=-Vmax;
-            } else if (Zvel>Vmax){
-                Zvel=Vmax;
+
+        // calculation of velocity and positon of x
+        if(Xvel!=-5){
+            XvelPlus = VelCalculator(VxFifo, command, Xvel);
+            if(XvelPlus==2){
+                Xvel=0;
+                XvelPlus=0;
+            } else {
+                Xvel=Xvel+XvelPlus;
+                if (Xvel<-Vmax){
+                    Xvel=-Vmax;
+                } else if (Xvel>Vmax){
+                    Xvel=Vmax;
+                }
             }
         }
-
-        Zpos=Zpos+Zvel;
-        if (Zpos<Zmin){
-            Zpos=Zmin;
-            Zvel=0;
-        } else if (Zpos>Zmax){
-            Zpos=Zmax;
-            Zvel=0;
+        
+        Xpos=Xpos+Xvel;
+        if (Xpos<Xmin){
+            Xpos=Xmin;
+            Xvel=0;
+        } else if (Xpos>Xmax){
+            Xpos=Xmax;
+            Xvel=0;
         }
 
-        SendPosition(PzFifo, Zpos);
+        SendPosition(PxFifo, Xpos);
 
         sleep(1);
     }
